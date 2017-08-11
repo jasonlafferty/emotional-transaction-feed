30ae4655dd275309ac7635f0a7e4e2b5c4f86c39
Jason Lafferty <jason_d_lafferty@hotmail.com>
2017-08-11 09:20:12 +0100

    Add sort by date for transitions

--- src/app/utils/__tests__/index.test.js
+++ src/app/utils/__tests__/index.test.js
@@ -1,4 +1,7 @@
-import { isStringContainedInStrings } from '../index';
+import {
+  isStringContainedInStrings,
+  sortTransitionsByDate,
+} from '../index';
 
 describe('isStringContainedInStrings', () => {
   it('Should return true if search is empty string', () => {
@@ -19,5 +22,30 @@ describe('isStringContainedInStrings', () => {
   it('Should return false if no matches', () => {
     const result = isStringContainedInStrings('hate', ['emotion', 'desCriPtion']);
     expect(result).toEqual(false);
+  });
+});
+
+describe('sortTransitionsByDate', () => {
+  it('Should sort transactions by time', () => {
+    const resultOne = [
+      {
+        id: 1,
+        created: '2017-07-11T14:08:26.192+00:00',
+      }, {
+        id: 2,
+        created: '2016-07-11T14:08:26.192+00:00',
+      }].sort(sortTransitionsByDate);
+
+    const resultTwo = [
+      {
+        id: 1,
+        created: '2016-07-11T14:08:26.192+00:00',
+      }, {
+        id: 2,
+        created: '2017-07-11T14:08:26.192+00:00',
+      }].sort(sortTransitionsByDate);
+
+    expect(resultOne[0].id).toEqual(1);
+    expect(resultTwo[0].id).toEqual(2);
   });
 });
--- src/app/utils/index.js
+++ src/app/utils/index.js
@@ -2,3 +2,6 @@ export const isStringContainedInStrings = (searchString = '', strings = []) => {
   const stringToSeach = strings.map(s => s.toLocaleLowerCase()).join();
   return (searchString === '' || stringToSeach.includes(searchString));
 };
+
+export const sortTransitionsByDate = ({ created: dateOne }, { created: dateTwo }) =>
+  new Date(dateTwo) - new Date(dateOne);
